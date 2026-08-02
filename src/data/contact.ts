57f555f5816d4a1cb279e231d41cbb9f440a127a
Amruthamsh A <amruthaamsh@gmail.com>
2026-08-03 00:40:42 +0530

    fixed whatsapp phone number

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,4 +1,4 @@
-export const whatsappNumber = "91 9900493944";
+export const whatsappNumber = "9900493944";
 export const phoneNumber = "+91 9900493944";
 export const email = "amruthamsh13@gmail.com";
 
